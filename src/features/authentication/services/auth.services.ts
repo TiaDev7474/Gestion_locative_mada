979064e158e70ef384397b8b68e0a8fce71db816
Nomenjanahary Riry <nomenjanaharyriry@gmail.com>
2023-10-15 21:43:26 +0300

    Update auth.services.ts , add new methode

--- src/features/authentication/services/auth.services.ts
+++ src/features/authentication/services/auth.services.ts
@@ -1,5 +1,13 @@
-class AuthService {
+import {InformationType} from "../../../types";
 
+class AuthService {
+    private _info: InformationType | undefined;
+    HandleClickCreate(info: InformationType) {
+        this._info = info;
+        console.log(this._info)
+
+
+    }
 }
 
 export const authService = new AuthService();
